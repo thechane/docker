e0a3671e00c7ee1b53d586d62df0a8c35bee22dd
Stuart Chaney <stu@macbook-pro.attlocal.net>
2017-05-27 18:49:06 -0500

    minor fixes to Python script

--- builds/soapui/remote/process.py
+++ builds/soapui/remote/process.py
@@ -9,18 +9,22 @@ try:
 except OSError:
     pass
 
-file = ' '.join(sys.argv[1:])
-splitFile = file.split('*')
-endpoint = splitFile.pop()
+fileString = ''.join(sys.argv[1:])
+endpoint = None
+if '*' in file:
+    splitFile = fileString.split('*')
+    endpoint = splitFile.pop()
+    #add more here if needed later
+    fileString = ''.join(splitFile)
 goXML = open("/tmp/go.xml", "w")
-goXML.write(gzip.GzipFile(fileobj=StringIO(base64.b64decode(''.join(splitFile)))).read())
+goXML.write(gzip.GzipFile(fileobj=StringIO(base64.b64decode(fileString))).read())
 goXML.close()
 
 try:
-    if endpoint == "null":
-        print check_output(["testrunner.sh", "-f", "/tmp", "-M", "/tmp/go.xml"], stderr=sys.stdout.fileno())
+    if endpoint is None:
+        print check_output(["testrunner.sh", "-Djava.awt.headless=true", "-f", "/tmp", "-M", "/tmp/go.xml"], stderr=sys.stdout.fileno())
     else:
-        print check_output(["testrunner.sh", "-f", "/tmp", "-M", "-e", endpoint, "/tmp/go.xml"], stderr=sys.stdout.fileno())
+        print check_output(["testrunner.sh", "-Djava.awt.headless=true", "-f", "/tmp", "-M", "-e", endpoint, "/tmp/go.xml"], stderr=sys.stdout.fileno())
 except CalledProcessError, e:
     resXML = open("/tmp/test_case_run_log_report.xml", "w")
     resXML.write("CalledProcessError during execution : " + str(e))
